cb10dc9f1f0e35db348d40d9c3f74f1d38810d6a
Karma King <2114747@talnet.nl>
2019-11-25 15:04:46 +0100

    derde geld automaat

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value;
+    if (saldo == 500) {
+        document.getElementById("tekst").innerHTML = "er is gestort ";
+    } else if (saldo > 500) {
+        document.getElementById("tekst").innerHTML = "er is gestort met bonus"
+    } else {
+        document.getElementById("tekst").innerHTML = "baas klappe niffauw"
+    }
+
+}
