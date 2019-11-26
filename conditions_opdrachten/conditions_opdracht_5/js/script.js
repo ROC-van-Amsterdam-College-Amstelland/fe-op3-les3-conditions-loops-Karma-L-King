142828a2bb63a73334b22add4c7ad9138fe34840
Karma King <2114747@talnet.nl>
2019-11-26 13:14:19 +0100

    calculator heeft nu plus en min

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,4 +1,13 @@
-//javascript code
-function min() {
+function check() {
+
+    var myNumber1 = parseInt(document.getElementById("myNumber1").value);
+    var myNumber2 = parseInt(document.getElementById("myNumber2").value);
+    var myOperator = document.getElementById("myOperator").value;
+
+    if (myOperator == "+") {
+        document.getElementById("tekst").innerHTML = myNumber1 + myNumber2;
+    } else if (myOperator == "-") {
+        document.getElementById("tekst").innerHTML = myNumber1 - myNumber2;
+    }
 
 }
