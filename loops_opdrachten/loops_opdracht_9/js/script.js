7c22f60ec9a1a2cda544e9f310070364ec65598f
Karma King <2114747@talnet.nl>
2019-12-03 15:02:57 +0100

    joejoe

--- loops_opdrachten/loops_opdracht_9/js/script.js
+++ loops_opdrachten/loops_opdracht_9/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+
+
+
+for (var i = 1; i < 6; i++) {
+    console.log(i);
+}
+console.log("Het einde is bereikt!");
+console.log("Het begin is het begin");
+for (var i = 1; i < 6; i++) {
+    console.log(i);
+}
